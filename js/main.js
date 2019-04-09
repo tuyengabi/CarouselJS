6f7799f8a67a50bf88fe1e5dfb02e92162610c20
tuyengabi <tuyen.gabi@gmail.com>
2019-04-09 16:23:53 +0200

    atelier

--- js/main.js
+++ js/main.js
@@ -1,14 +1,27 @@
 "use strict";
 
-let lis = document.getElementsByTagName('li');
+/*let lis = document.getElementsByTagName('li');
 console.log(typeof(lis));
 
-let imgs = document.getElementsByTagName('img');
+let imgs = document.getElementsByClassName('.carousel__img');
 console.log(imgs);
+
 
 function carousel() {
 
-	for(let img of imgs) {
-		img.style.display = "none";
+}*/
+
+let count = 0;
+
+function imgNext() {
+	let imgs = document.getElementsByClassName('carousel__img');
+	
+	if(count >= imgs.length - 1) {
+	count = 0;
+	imgs[count].classList.remove('displayNone');
+	} else {
+	imgs[count].classList.add('displayNone');
+	imgs[count+1].classList.remove('displayNone');
+	count++;
 	}
 }
